d09a04898769732bbd38ef4d7aed4e4a7a59b6bc
EliasRamil <raeli0072@gmail.com>
2025-02-06 18:42:34 -0300

    limpieza

--- src/app.py
+++ src/app.py
@@ -1,11 +1,7 @@
-import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
 from src.database.db_sqlite import db, url_db
 from src.models.tarea import Tarea
-
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -16,5 +12,3 @@ db.init_app(app)
 
 migrate = Migrate()
 migrate.init_app(app, db)
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
